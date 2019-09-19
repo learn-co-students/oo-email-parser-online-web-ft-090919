b603acf15e5875bdcf482c7ea53466267a9627d4
Daniel Lechok <dclechok@gmail.com>
2019-09-19 00:57:02 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,14 +8,6 @@ class EmailAddressParser
   
   def parse() #parse emails into an array of unique addresses
     new_email_array = []
-		new_email_array = @@emails.split(/[, ]/) 
+		new_email_array = @@emails.split(/[, ]/).reject(&:empty?).uniq
   end
 end
-
-email_addresses = "john@doe.com, person@somewhere.org, person@somewhere.org"
-# email_addresses = "john@doe.com person@somewhere.org jon@asos.com"
-parser = EmailAddressParser.new(email_addresses)
-parser.parse
-
-
-# (/[, ]/) -> works for space delimited - does not remove "" empty elements
